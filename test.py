c8146d00d4177f3832f08aaee49509d5057b5a44
Ryan <cumt.xiaochi@gmail.com>
2014-06-17 16:32:24 +0800

    test multi line

--- test.py
+++ test.py
@@ -3,22 +3,39 @@ from scheme import evalue_code
 import glob  
   
 def equal(result, expected):
-    if str(rs) != output:
-        float_output = output+'.0'
-        print(str(rs), float_output)
-        if str(rs) == float_output:
-            print('Warn, it shoud be int, not float', output)
+    if str(result) != expected:
+        float_output = expected+'.0'
+        print(str(result), float_output)
+        if str(result) == float_output:
+            print('Warn, it shoud be int, not float', expected)
             return True
         else:
             return False
     return True
 
-file_list = glob.iglob(r'test/*.code')
+def test():
 
-for f in file_list:
-    fh = open(f)
-    ofh = open(f+'.output')
-    for code in fh.readlines():
+    file_list = glob.iglob(r'test/*.code')
+
+    for f in file_list:
+        fh = open(f)
+        ofh = open(f+'.output')
+        for code in fh.readlines():
+            print(code)
+            rs = evalue_code(code)
+            print(rs)
+            output = ofh.readline().strip()
+            print(str(rs), output)
+            if not equal(rs, output):
+                print('Error, result', rs, 'expected', output)
+                return False
+
+    file_list = glob.iglob(r'test/*.sc')
+
+    for f in file_list:
+        fh = open(f)
+        ofh = open(f+'.output')
+        code = ''.join(fh.readlines())
         print(code)
         rs = evalue_code(code)
         print(rs)
@@ -26,14 +43,13 @@ for f in file_list:
         print(str(rs), output)
         if not equal(rs, output):
             print('Error, result', rs, 'expected', output)
-            break
+            return False
+    return True
 
-#evalue_code('(+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6))')
-# evalue_code('''(+ (* 3
-#       (+ (* 2 4)
-#          (+ 3 5)))
-#    (+ (- 10 7)
-#       6))''')
+if not test():
+    print('Fail!')
+else:
+    print('Pass!')
 
 #evalue_code('(define size 2)')
 #evalue_code('size')
