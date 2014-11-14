f1f195c1e810ee17f097b2fc17f9f11f85157630
Ryan <cumt.xiaochi@gmail.com>
2014-11-15 00:00:00 +0800

    we change it to class
    more comfortable

--- scanner.py
+++ scanner.py
@@ -3,11 +3,38 @@ import logger
 log = logger.Log()
 log.debug_ = True
 
+class Tree(object):
+    """docstring for Tree"""
+    def __init__(self):
+        super(Tree, self).__init__()
+        self.left = None
+        self.right = None
+
+class Token(object):
+    """docstring for Token"""
+    def __init__(self, token):
+        super(Token, self).__init__()
+        self.token = token
+        
+class Number(object):
+    """docstring for Number"""
+    def __init__(self, value):
+        super(Number, self).__init__()
+        self.value = value
+
+class String(object):
+    """docstring for String"""
+    def __init__(self, text):
+        super(String, self).__init__()
+        self.text = text
+
+def parse_token(liter):
+    return Token(liter)
+
 # key: cur_state
 # value: cur_char, next_state, callback
 # callback(char, literal) => (literal, token_list, ErrorMessage, cmd)
 # cmd=push|pop
-# token is pure string, and string is ("string", "xxxx"), and digit is digit
 
 when_normal = [
     [lambda c: c == '(', 'normal', lambda c, i: (None, None, None, 'push')],
@@ -15,7 +42,7 @@ when_normal = [
     [lambda c: c.isspace(), 'normal', lambda c, i: (None, None, None)],
     [lambda c: c == '"', 'string', lambda c, i: ('', None, None)],
     [lambda c: True, 'token', lambda c, i: (c, None, None)],
-    ]
+]
 when_token = [
     [lambda c: c.isalpha() or c.isdigit() or c in '.-_?<>', 'token', lambda c, i: (i+c, None, None)],
     [lambda c: c.isspace(), 'normal', lambda c, i: (None, i, None)],
@@ -75,6 +102,148 @@ def token2value(token):
             raise Exception(token+' is not boolean')
     return token
 
+class Scanner(object):
+    """docstring for Scanner"""
+    def __init__(self):
+        super(Scanner, self).__init__()
+
+        self.ERROR = -1
+        self.NORMAL = 1
+        self.TOKEN = 2
+        self.STRING = 3
+        self.ESCAPE = 4
+
+        _normal = [
+            [lambda c: c == '(', self.NORMAL, self.start_list],
+            [lambda c: c == ')', self.NORMAL, self.end_list],
+            [lambda c: c.isspace(), self.NORMAL, self.do_nothing],
+            [lambda c: c == '"', self.STRING, self.start_string],
+            [lambda c: True, self.TOKEN, self.start_token],
+        ]
+        is_token_char = lambda c: c.isalpha() or c.isdigit() or c in '-_?<>'
+        _token = [
+            [is_token_char, self.TOKEN, self.on_token],
+            [lambda c: c.isspace(), self.NORMAL, self.end_token],
+            [lambda c: c == ')', self.NORMAL, self.end_list],
+            [lambda c: True, self.ERROR, self.do_nothing],
+        ]
+        _string = [
+            [lambda c: c == '"', self.NORMAL, self.end_string],
+            [lambda c: c == '\\', self.ESCAPE, self.do_nothing],
+            [lambda c: True, self.STRING, self.on_string],
+        ]
+        _escape = [
+            [lambda c: True, self.STRING, self.on_escape],
+        ]
+        self.trans_table = [
+            self.NORMAL: _normal,
+            self.TOKEN: _token,
+            self.STRING: _string,
+            self.ESCAPE: _escape,
+        ]
+        self.escape_table = escape_table
+        self.char = None
+        self.cur_list = None
+        self.stack = []
+        self.is_right = False
+
+    def scan(code):
+        self.state = self.NORMAL
+        self.token_list_tree = None
+        self.ast = None
+        for c in code:
+            self.eat_char(c)
+        return ast
+        
+    def eat_char(self, c):
+        self.char = c
+        for e in self.trans_table[self.state]:
+            if self.match(c, e[0]):
+                next_state = e[1]
+                e[2]()
+                break
+    
+    def do_nothing(self):
+        pass
+
+    def start_list(self):
+        if self.cur_list is not None:
+            self.stack.append(self.cur_list)
+        self.cur_list = List()
+
+    def end_list(self):
+        _list = self.cur_list
+        self.cur_list = self.stack.pop()
+        self.cur_list.right = _list
+
+    def start_token(self):
+        self.cur_token = ''
+        self.on_token()
+
+    def on_token(self):
+        self.cur_token += self.char
+
+    def end_token(self):
+        self.cur_list.append(Token(self.cur_token))
+
+    def start_string(self):
+        self.cur_string = ''
+
+    def on_string(self):
+        self.cur_string += self.char
+
+    def end_string(self):
+        self.cur_list.append(String(self.cur_string))
+
+    def on_escape(self):
+        c = self.char
+        if self.char in self.escape_table:
+            c = self.escape_table[self.char]
+        self.cur_string += c
+
+class Translator(object):
+    """docstring for Translator"""
+    def __init__(self, token_list_tree):
+        super(Translator, self).__init__()
+        self.token_list_tree = token_list_tree
+        self.is_right = False
+        self.cur_tree = None
+        self.stack = []
+
+    def translate(self):
+        if isinstance(t, list):
+            return self.eat_list(t)
+        if isinstance(t, Token):
+            return self.trans_token(t)
+        return t
+
+    def self.eat_list(self, lst):
+        if len(lst) == 0:
+            return Tree()
+        root = None
+        last_tree = None
+        for t in lst:
+            if isinstance(t, Token):
+                if self.is_dot(Token):
+                    assert not self.is_right, 'there are 2 dots'
+                    assert root is not None
+                    assert last_tree is not None
+                    self.is_right = True
+                    continue
+                t = self.trans_token(t)
+            if self.is_right:
+                assert last_tree is not None
+                assert last_tree.right is None, 'right most more than one'
+                last_tree.right = t
+            else:
+                tree = Tree
+                tree.left = t
+                if root is None:
+                    root = tree
+                if last_tree:
+                    last_tree.right = tree
+                last_tree = tree
+        
 '''
 consume a char, and build, but it can not build tree 
 pre-condition:
